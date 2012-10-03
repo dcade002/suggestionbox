5554c7e2baf647e0f16234f3fb681fdc76683cba
Jose Infante <jinfa006@fiu.edu>
2012-10-02 20:20:37 -0400

    Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
   	attr_accessible :email, :name
+
+  	validates :name, presence: true, length: {maximum: 20}
+ 
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
