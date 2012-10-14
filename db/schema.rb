79c41c255fb1d6242959502049f10463b0914152
Leo Correa <lcorr005@gmail.com>
2012-10-14 18:53:55 -0400

    trying to fix migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121014203018) do
+ActiveRecord::Schema.define(:version => 20121014225204) do
 
   create_table "posts", :force => true do |t|
     t.text     "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121014203018) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
