4c5139390cb3beab44bc3253fad7cdebc4178919
Leo Correa <lcorr005@gmail.com>
2012-10-14 10:54:28 -0400

    Added fix for heroku and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Suggestionbox::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Fix for heroku and devise
+  config.assets.initialize_on_precompile = false
+  
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
