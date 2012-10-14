2c7c89791668a4e5e77f3e8498b99a52a11b2617
Leo Correa <lcorr005@gmail.com>
2012-10-14 16:50:53 -0400

    Some users?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -9,4 +10,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   before_save { self.email.downcase! }
+  before_save :create_remember_token
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
